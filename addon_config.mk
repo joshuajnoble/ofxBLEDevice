8682fcc50ec6f061f6d842ffc0f07855a7398a5c
Dimitre <dimitre.github@mailnull.com>
2016-10-08 21:41:20 -0300

    Project generator will add CoreBluetooth
    CoreBluetooth automatically added for new projects

---
+++ addon_config.mk
@@ -0,0 +1,9 @@
+meta:
+	ADDON_NAME = ofxBLEDevice
+	ADDON_DESCRIPTION = ofxBLEDevice
+	ADDON_AUTHOR = ofxBLEDevice
+	ADDON_TAGS = "BLE"
+	ADDON_URL = ofxBLEDevice
+
+common:
+	ADDON_FRAMEWORKS = CoreBluetooth
